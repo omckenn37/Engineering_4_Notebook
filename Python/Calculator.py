b99972dc076872f0e3e8019437753258d3fd6ec0
omckenn37 <omckenn37@charlottesvilleschools.org>
2020-11-05 19:27:19 +0000

    commented code

--- Python/Calculator.py
+++ Python/Calculator.py
@@ -1,19 +1,20 @@
-def doMath(a, b, function):
-    if function == 1:
-        return str(a + b)
-    elif function == 2:
+def doMath(a, b, operation): # the doMath() function takes inputs a, b, and operation. a is the first number, b is the second number, and operation refers to what operation needs to be performed
+    # this series of if statements checks to see what operation needs to be performed
+    if operation == 1:
+        return str(a + b) # if the input is 1, the doMath() function will return the sum of a and b as a string
+    elif operation == 2:
         return str(a - b)
-    elif function == 3:
+    elif operation == 3:
         return str(a * b)
-    elif function == 4:
-        return str(round(a / b, 2))
+    elif operation == 4:
+        return str(round(a / b, 2)) # to round numbers in python, you do round(number, digits)
     else:
         return str(a % b)
 
-a = int(input("Enter first number : "))
+a = int(input("Enter first number : ")) # these two lines get the user input
 b = int(input("Enter second number : "))
 
-print("Sum : " + doMath(a,b,1))
+print("Sum : " + doMath(a,b,1)) # these lines print out the sum, differene, product, quotient, and modulo of the numbers
 print("Difference : " + doMath(a,b,2))
 print("Product : " + doMath(a,b,3))
 print("Quotient : " + doMath(a,b,4))
