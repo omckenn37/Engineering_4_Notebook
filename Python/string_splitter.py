1ab1f2884b9f562f85386c1c90b402181ab33d14
omckenn37 <omckenn37@charlottesvilleschools.org>
2020-11-12 19:12:49 +0000

    added comments to string_splitter.py

--- Python/string_splitter.py
+++ Python/string_splitter.py
@@ -1,11 +1,12 @@
 # String Splitter
 # Program by Owen McKenney
 
-string = str(input("Enter a sentence: "))
+string = str(input("Enter a sentence: ")) # gets user input
+string = string.replace(" ", "-") # replaces whitespaces with -
+string_list = list(string) # converts string to list
+string_list.append("-") # adds - at end
 
-for x in string:
-    if x == " ":
-        print("-")
-    else:
-        print(x)
+for x in string_list: # prints all elements in the list
+    print(x)
+
 
