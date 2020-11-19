d4dbfb2837c507af1d882ecec5afc2f4576b6f71
omckenn37 <omckenn37@charlottesvilleschools.org>
2020-11-19 19:12:08 +0000

    changed hangman.py

--- Python/hangman.py
+++ Python/hangman.py
@@ -2,7 +2,7 @@
 # Program by Owen McKenney
 
 word = list(input("Player 1, what's the word? ").lower())
-print("\n" * 10)
+print("\n" * 20)
 
 guess_list = []
 
@@ -11,14 +11,17 @@ for i in range(0, len(word)):
 
 print(guess_list)
 
-#while guess_list != word:
-
 while guess_list != word:
-    guess = str(input("\nPlayer 2, guess a letter: ").lower())
+    valid_guess = False
     contains = False
+    guess = str(input("\nPlayer 2, guess a letter: ").lower())
 
+    for t in guess_list:
+        if guess == t:
+            valid_guess = True
+           
     for i in word:
-        if(i == guess):
+        if i == guess and valid_guess == False:
             contains = True
     
     if contains == True:
@@ -27,14 +30,13 @@ while guess_list != word:
         for j in range(0, len(indices)):
             guess_list.insert(indices[j], guess)
             guess_list.pop(indices[j] + 1)
-
         
         for x in range(0, len(guess_list)):
             print(guess_list[x], end=" ")
-
             
-    else:
+    elif valid_guess == False:
         print("Guess again!")
-
+    else:
+        print("You already guessed that letter")
        
 
