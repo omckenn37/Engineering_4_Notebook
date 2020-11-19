3001ab67fa3134287972006bd5fc1b15a676e54e
omckenn37 <omckenn37@charlottesvilleschools.org>
2020-11-19 18:43:22 +0000

    changed hangman.py

--- Python/hangman.py
+++ Python/hangman.py
@@ -14,17 +14,27 @@ print(guess_list)
 #while guess_list != word:
 
 while guess_list != word:
-    guess = str(input("Player 2, guess a letter: ").lower())
+    guess = str(input("\nPlayer 2, guess a letter: ").lower())
+    contains = False
 
     for i in word:
         if(i == guess):
-            indices = [i for i, x in enumerate(word) if x == guess]
+            contains = True
+    
+    if contains == True:
+        indices = [i for i, x in enumerate(word) if x == guess]
 
-            for j in range(0, len(indices)):
-                guess_list.insert(indices[j], guess)
-                guess_list.pop(indices[j] + 1)
+        for j in range(0, len(indices)):
+            guess_list.insert(indices[j], guess)
+            guess_list.pop(indices[j] + 1)
 
-            print(guess_list)
+        
+        for x in range(0, len(guess_list)):
+            print(guess_list[x], end=" ")
+
+            
+    else:
+        print("Guess again!")
 
        
 
