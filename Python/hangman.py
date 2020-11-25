4340a41e7841c1e5581746dd925f1cec81662282
omckenn37 <omckenn37@charlottesvilleschools.org>
2020-11-25 19:06:58 +0000

    hangman almost working

--- Python/hangman.py
+++ Python/hangman.py
@@ -6,35 +6,45 @@ print("\n" * 40)
 
 guessed = "_" * len(word)
 guessed = list(guessed)
-first_guessed = []
-letter = input("Player 2, guess a letter: ")
+already_guessed = []
+letter = ""
 num_guesses = 5
+print("Player 2, try to guess the word before Graham's face is printed out")
+
+def draw(n):
+    f = open("graham.txt", "r")
+    for x in range(0, (5-n) * 4 + 1):
+        print(f.readline(), end="")
 
 while True:
-    if letter.upper() in first_guessed:
+    print("-" * 40)
+    letter = input("Player 2, guess a letter: ")
+
+    if letter.upper() in already_guessed:
         letter = ""
         print("Player 2, you already guessed this letter")
+
     elif letter.upper() in word:
+        already_guessed.append(letter.upper())
         index = word.index(letter.upper())
         guessed[index] = letter.upper()
         word[index] = "_"
+        print("".join(guessed))
 
-    else:
+    elif letter.upper() not in word:
+        already_guessed.append(letter.upper())
         num_guesses -= 1
         print("Wrong! Guesses left: " + str(num_guesses))
-        print("".join(guessed))
+        print("")
+        draw(num_guesses)
+        print("")
+
         if num_guesses == 0:
             print("Player 2, you ran out of guesses. You lose!")
-            break
+            break            
         
-        if letter is not "":
-            first_guessed.append(letter.upper())
-        letter = input("Player 2, guess a letter: ")
-
     if "_" not in guessed:
-        print(word)
         print("Player 2, you won")
-
         break
-       
+    
 
