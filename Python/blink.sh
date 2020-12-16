3973647c73004fde07e43b18821ba76706420328
omckenn37 <omckenn37@charlottesvilleschools.org>
2020-12-16 22:19:42 +0000

    fixed and commented blink.sh

--- Python/blink.sh
+++ Python/blink.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-for i in {1..10}
-do
-	echo "1" > /sys/class/gpio/gpio2/value
-	sleep 1
-	echo "0" > /sys/class/gpio/gpio2/value
-	sleep 1
+
+for i in {1..20} # for loop that runs 20 times
+do	
+	# for simplicity, I wired both LED's to one pin
+	/usr/bin/gpio toggle 29 # this toggles gpio29 on and off
+	sleep .5	
 done
+
