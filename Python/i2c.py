fc1509574f5a2bb1dc27d4d422ceeb8895c7cedc
omckenn37 <omckenn37@charlottesvilleschools.org>
2020-12-24 17:02:16 +0000

    commented i2c.py

--- Python/i2c.py
+++ Python/i2c.py
@@ -7,8 +7,8 @@ from PIL import ImageDraw
 from PIL import ImageFont
 
 RST = 24
-accelerometer = Adafruit_LSM303.LSM303()
-disp = Adafruit_SSD1306.SSD1306_128_64(rst=RST, i2c_address=0x3d)
+accelerometer = Adafruit_LSM303.LSM303() # accelerometer setup
+disp = Adafruit_SSD1306.SSD1306_128_64(rst=RST, i2c_address=0x3d) # dislay setup
 disp.begin()
 disp.clear()
 disp.display()
@@ -17,9 +17,10 @@ width = disp.width
 height = disp.height 
 image = Image.new('1', (width, height))
 
-draw = ImageDraw.Draw(image)
-draw.rectangle((0,0,width,height), outline=0, fill=0)
+draw = ImageDraw.Draw(image) # gets drwaing object to draw on image
+draw.rectangle((0,0,width,height), outline=0, fill=0) # clears screen
 
+# variables to help drawing
 padding = 2
 shape_width = 20
 top = padding
@@ -27,19 +28,19 @@ bottom = height - padding
 x = padding
 font = ImageFont.load_default()
 
-#draw.ellipse((x, top, x + shape_width, bottom), outline=255, fill=0)
-
-#disp.image(image)
-#disp.display()
-
-
-
 while True:
-	accel, mag = accelerometer.read()
+	accel, mag = accelerometer.read() # gets accelerometer data
 	accel_x, accel_y, accel_z = accel
-	mag_x, mag_y, mag_z = mag
+	
+	draw.text((x, top), "Accelerometer Data:", font=font, fill=255) # draws header
+	draw.text((x, top + 10), "Accel x ={0}".format(round(accel_x / 100, 3)), font=font, fill=255) # draws x 
+	draw.text((x, top + 20), "Accel y ={0}".format(round(accel_y / 100, 3)), font=font, fill=255) # draws y
+	draw.text((x, top + 30), "Accel z ={0}".format(round(accel_z / 100, 3)), font=font, fill=255) # draws z
 
-	draw.text((x, top), "Accel x ={0}".format(accel_x), font=font, fill=255)
+	disp.image(image) # displays x, y, z, and header
+	disp.display()
+	
+	draw.rectangle((100, 12, 55, 50), outline=0, fill=0) # "refreshes" the xyz values so they can be updated
 	disp.image(image)
 	disp.display()
 	
