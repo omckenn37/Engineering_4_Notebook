121954fc34ffcd79734355fe45dd50121af460c8
omckenn37 <omckenn37@charlottesvilleschools.org>
2021-01-19 18:44:30 +0000

    fixed headless

--- Python/headless.py
+++ Python/headless.py
@@ -25,21 +25,23 @@ font = ImageFont.load_default()
 disp.image(image)
 disp.display() # clears screen
 
+radius = 5
+
 while True:
 	draw.rectangle((0, 0, width, height), outline=0, fill=0) # draws black rectangle on screen
 	accel, mag = accelerometer.read() # gets accelerometer data
 	accel_x, accel_y, accel_z = accel
 	mag_x, mag_y, mag_z = mag
 	
-	# these lines get the x and y position for the dot based on the accelerometer values
-	x_pos = 128 - (accel_y / 100 + 9.8) / 19.6 * 128 
-	y_pos = 64 - (accel_x / 100 + 9.8) / 19.6 * 64 
-	
+	# these lines get the x and y position for the dot based on the accelerometer values, more info on these in the readme
+	x_pos = 128 - (accel_y / 100) / 15 * 128 - 64 
+	y_pos = 64 - (accel_x / 100) / 15 * 64 - 32
+		
 	#print(x_pos, y_pos)
 	# if you uncomment this line, you can see that when the accelerometer is in a neutral position,
 	# the x and y position values hover around 63 and 33, which is extremely close to dead center
 
-	draw.ellipse((x_pos - 5, y_pos - 5, x_pos + 5, y_pos + 5), outline=255, fill=255) # draws the dot that moves around
+	draw.ellipse((x_pos - radius, y_pos - radius, x_pos + radius, y_pos + radius), outline=255, fill=255) # draws the dot that moves around
 	
 	disp.image(image)
 	disp.display() # displays the dot
